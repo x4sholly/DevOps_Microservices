2778b7a288e42bf8aceb2cef043215f0836f87d8
EC2 Default User <ec2-user@ip-172-31-93-41.ec2.internal>
2022-08-26 03:04:54 +0000

    Project4 pushed to Github

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="x4sholly/microproject"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microproject --image=x4sholly/microproject --port=80 --labels app=microproject
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward microproject 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -7,9 +7,14 @@
 # Create dockerpath
 # dockerpath=<your docker ID/path>
 
+dockerpath="x4sholly/microproject"
+
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag microproject $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
